74845e2af299fe4f098fbf548c3b33b248e4f33a
quang.nguyenmanh <quang.nguyenmanh@vndirect.com.vn>
2019-10-23 11:21:53 +0700

    a

--- src/TodoBanner.js
+++ src/TodoBanner.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 export class TodoBanner extends Component {
     render = () =>
         <h4 className="bg-primary text-white text-center p-2">
-            {this.props.name}'s Quạt bánh đa
+            {this.props.name}'s Quạt bánh đa 1
             ({this.props.tasks.filter(t => !t.done).length} items to do)
         </h4>
 }
